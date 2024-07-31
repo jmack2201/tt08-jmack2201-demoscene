a5b04d4fb61769478758c15b3bb0fb833e8d35f5
Jacob Mack <jacobmack2@live.com>
2024-07-31 16:17:05 -0700

    created blank modules for audio_source and pwm

---
+++ src/audio_source.v
@@ -0,0 +1,7 @@
+module audio_source (
+    input clk, rst_n,
+    input [1:0] audio_select,
+    output audio_out
+);
+    
+endmodule
---
+++ src/pwm.v
@@ -0,0 +1,6 @@
+module pwm (
+    input clk, rst_n, audio_in,
+    output pwm_out
+);
+    
+endmodule
