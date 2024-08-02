832a9d36971be4b892fe7ec047d7135328abd27e
Jacob Mack <jacobmack2@live.com>
2024-08-01 22:59:40 -0700

    quick bug fix for pwm

--- src/pwm.v
+++ src/pwm.v
@@ -1,13 +1,13 @@
 module pwm (
     input clk, rst_n, audio_in,
     input [7:0] sample,
-    output pwm_out
+    output reg pwm_out
 );
     reg [7:0] pwm_count;
     always @(posedge clk ) begin
         if (!rst_n) begin
             pwm_count <= 0;
-            pwm <= 0;
+            pwm_out <= 0;
         end else begin
             pwm_out <= pwm_count < sample;
 
