eb5877c50cd39ae9db1f46720f693376f0f2c955
Jacob Mack <jacobmack2@live.com>
2024-08-11 21:40:09 -0700

    started sprite rom and pixel output

---
+++ src/sprite_rom.v
@@ -0,0 +1,10 @@
+module sprite_rom (
+    input clk,
+    input [7:0] addr,
+    output [7:0] color_out
+);
+    reg [7:0] color_arr [0:7];
+    always @(posedge clk ) begin
+        color_out <= color_arr[addr];
+    end
+endmodule
