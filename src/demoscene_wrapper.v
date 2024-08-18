9c61ea35fef1f63a987754ecc7300a2f1bdae44e
Jacob Mack <jacobmack2@live.com>
2024-08-18 15:40:51 -0600

    switched to using TT hvsync generator

--- src/demoscene_wrapper.v
+++ src/demoscene_wrapper.v
@@ -3,43 +3,49 @@ module demoscene_wrapper (
     output [1:0] vga_r, vga_b, vga_g,
     output hsync, vsync, pwm_out, MISO
 );
-    reg [10:0] h_count, v_count;
+    reg [9:0] h_count, v_count;
     reg visible;
 
-    vga vga (
+    hvsync_generator vga (
         .clk(clk),
-        .rst_n(rst_n),
-        .h_count(h_count),
-        .v_count(v_count),
-        .visible(visible),
+        .reset(~rst_n),
+        .hpos(h_count),
+        .vpos(v_count),
+        .display_on(visible),
         .hsync(hsync),
         .vsync(vsync)
     );
 
-    spi spi (
-        .SCLK(SCLK),
-        .SSEL(SSEL),
-        .MOSI(MOSI),
-        .MISO(MISO)
-    );
+    // spi spi (
+    //     .SCLK(SCLK),
+    //     .SSEL(SSEL),
+    //     .MOSI(MOSI),
+    //     .MISO(MISO)
+    // );
 
-    reg audio_source_out;
+    // reg audio_source_out;
 
-    pwm pwm (
-        .clk(clk),
-        .rst_n(rst_n),
-        .sample(8'h01),
-        .pwm_out(pwm_out)
-    );
+    // pwm pwm (
+    //     .clk(clk),
+    //     .rst_n(rst_n),
+    //     .sample(8'h01),
+    //     .pwm_out(pwm_out)
+    // );
 
     pixel_color pixel (
         .clk(clk),
+        .visible(visible),
+        .hpos(h_count),
+        .vpos(v_count),
+        .hsync(hsync),
+        .vsync(vsync),
+        .rst_n(rst_n),
         .vga_color({vga_r,vga_g,vga_b})
     );
 
-    audio_source audio_source(
-        .clk(clk),
-        .rst_n(rst_n),
-        .audio_out(audio_source_out)
-    );
+    // audio_source audio_source(
+    //     .clk(clk),
+    //     .rst_n(rst_n),
+    //     .audio_out(audio_source_out)
+    // );
 endmodule
--- src/vga_cfg.v
+++ src/vga_cfg.v
@@ -1,19 +1,19 @@
-parameter H_VISIBLE_S = 0;
-parameter H_FRONT_S = 1;
-parameter H_PULSE_S = 2;
-parameter H_BACK_S = 3;
+// parameter H_VISIBLE_S = 0;
+// parameter H_FRONT_S = 1;
+// parameter H_PULSE_S = 2;
+// parameter H_BACK_S = 3;
 
-parameter V_VISIBLE_S = 0;
-parameter V_FRONT_S = 1;
-parameter V_PULSE_S = 2;
-parameter V_BACK_S = 3;
+// parameter V_VISIBLE_S = 0;
+// parameter V_FRONT_S = 1;
+// parameter V_PULSE_S = 2;
+// parameter V_BACK_S = 3;
 
-parameter H_VISIBLE_C = 640;
-parameter H_FRONT_C = 16;
-parameter H_PULSE_C = 96;
-parameter H_BACK_C = 48;
+parameter H_DISPLAY = 640;
+parameter H_FRONT = 16;
+parameter H_SYNC = 96;
+parameter H_BACK = 48;
 
-parameter V_VISIBLE_C = 480;
-parameter V_FRONT_C = 10;
-parameter V_PULSE_C = 2;
-parameter V_BACK_C = 33;
+parameter V_DISPLAY = 480;
+parameter V_BOTTOM = 10;
+parameter V_SYNC = 2;
+parameter V_TOP = 33;
