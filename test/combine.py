953b594851268106aa8a1d2ccf4de6c65a918da4
Jacob Mack <jacobmack2@live.com>
2024-08-21 11:49:49 -0400

    moved combine.py to test folder

--- test/combine.py
+++ test/combine.py
@@ -9,7 +9,7 @@ all_v = [v_file for v_file in os.listdir(".") if ".v" in v_file]
 display = ["vga_cfg.v","tt_um_jmack2201.v", "demoscene_wrapper.v", "hvsync_generator.v", "pixel_color.v"]
 
 for verilog_file in display:
-    with open(verilog_file,"r") as fp:
+    with open("../src/"+verilog_file,"r") as fp:
         if verilog_file == "tt_um_jmack2201.v":
             v_contents = fp.read().replace("tt_um_jmack2201","tt_um_vga_example") + "\n"
         else:
