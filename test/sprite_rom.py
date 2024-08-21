4372a6caf97186b0d6d5f8ce6bec15ad8bc0ec1c
Jacob Mack <jacobmack2@live.com>
2024-08-21 15:02:56 -0400

    parametized the sprite rom script

--- test/sprite_rom.py
+++ test/sprite_rom.py
@@ -1,9 +1,12 @@
-header = "module sprite_rom (\n\tinput clk,\n\tinput [10:0] addr,\n\toutput reg [5:0] color_out\n);\n\treg [7:0] color_arr [2047:0];\n"
+with open("../src/params.v","r") as fp:
+    contents = fp.readlines()
+sprite_size = int([line.strip().split()[-1][:-1] for line in contents if "SPRITE" in line][0])
+header = "module sprite_rom (\n\tinput clk,\n\tinput [10:0] addr,\n\toutput reg [5:0] color_out\n);\n\treg [7:0] color_arr [SPRITE_SIZE*SPRITE_SIZE-1:0];\n"
 body = ""
 footer = "\n\talways @(posedge clk) begin\n\t\tcolor_out <= color_arr[addr][5:0];\n\tend\nendmodule\n"
 
 body += "\n\tinitial begin\n"
-for i in range(2048):
+for i in range(sprite_size*sprite_size):
     body += f"\t\tcolor_arr[{i}] = 8\'h0F;\n"
 body += "\tend\n"
 
