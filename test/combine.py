a16eb4cb22ea149f3079f2a44f53416a570f9fca
Jacob Mack <jacobmack2@live.com>
2024-08-25 21:33:09 -0400

    fixed file pointer

--- test/combine.py
+++ test/combine.py
@@ -6,7 +6,7 @@ if os.path.isfile("vga_test.v"):
     os.system("rm vga_test.v;touch vga_test.v")
 
 all_v = [v_file for v_file in os.listdir(".") if ".v" in v_file]
-display = ["vga_cfg.v","tt_um_jmack2201.v", "demoscene_wrapper.v", "hvsync_generator.v", "pixel_color.v", "sprite_rom.v", "spi.v", "audio_source.v", "sfx_rom.v"]
+display = ["params.v","tt_um_jmack2201.v", "demoscene_wrapper.v", "hvsync_generator.v", "pixel_color.v", "sprite_rom.v", "spi.v", "audio_source.v", "sfx_rom.v"]
 
 for verilog_file in display:
     with open("../src/"+verilog_file,"r") as fp:
