b2687b2b4cceb83563183abb3219e7d4767d8916
Jacob Mack <jacobmack2@live.com>
2024-09-05 02:22:45 -0400

    change back to 8 bit blocks in ROMs

--- test/sprite_rom.py
+++ test/sprite_rom.py
@@ -8,7 +8,7 @@ sprite_size = int([line.strip().split()[-1][:-1] for line in contents if "SPRITE
 num_addr_bits = math.ceil(math.log2(sprite_size*sprite_size))
 header = f"module sprite_rom0 (\n\tinput clk,\n\tinput [{num_addr_bits-1}:0] addr,\n\toutput reg [5:0] color_out\n);\n\treg [5:0] color_arr [SPRITE_SIZE*SPRITE_SIZE-1:0];\n"
 body = ""
-footer = "\n\talways @(posedge clk) begin\n\t\tcolor_out <= color_arr[addr];\n\tend\nendmodule\n"
+footer = "\n\talways @(posedge clk) begin\n\t\tcolor_out <= color_arr[addr][5:0];\n\tend\nendmodule\n"
 
 sprite = PIL.Image.open("rom0.png").convert("RGB")
 pixels = sprite.load()
@@ -20,7 +20,7 @@ for y in range(sprite_size):
     for x in range(sprite_size):
         lower_bits = [round(color/255 * 3) for color in pixels[x,y]]
         bit_string = "".join(f"{bits:02b}" for bits in lower_bits)
-        body += f"\t\tcolor_arr[{y*sprite_size+x}] = 6\'b{bit_string};\n"
+        body += f"\t\tcolor_arr[{y*sprite_size+x}] = 8\'b00{bit_string};\n"
 body += "\tend\n"
 
 with open("../src/sprite_rom0.v","w") as fp:
@@ -29,7 +29,7 @@ with open("../src/sprite_rom0.v","w") as fp:
 sprite_size = int([line.strip().split()[-1][:-1] for line in contents if "SPRITE" in line][0])
 header = f"module sprite_rom1 (\n\tinput clk,\n\tinput [{num_addr_bits-1}:0] addr,\n\toutput reg [5:0] color_out\n);\n\treg [5:0] color_arr [SPRITE_SIZE*SPRITE_SIZE-1:0];\n"
 body = ""
-footer = "\n\talways @(posedge clk) begin\n\t\tcolor_out <= color_arr[addr];\n\tend\nendmodule\n"
+footer = "\n\talways @(posedge clk) begin\n\t\tcolor_out <= color_arr[addr][5:0];\n\tend\nendmodule\n"
 
 sprite = PIL.Image.open("rom1.png").convert("RGB")
 pixels = sprite.load()
@@ -41,7 +41,7 @@ for y in range(sprite_size):
     for x in range(sprite_size):
         lower_bits = [round(color/255 * 3) for color in pixels[x,y]]
         bit_string = "".join(f"{bits:02b}" for bits in lower_bits)
-        body += f"\t\tcolor_arr[{y*sprite_size+x}] = 6\'b{bit_string};\n"
+        body += f"\t\tcolor_arr[{y*sprite_size+x}] = 8\'b00{bit_string};\n"
 body += "\tend\n"
 
 with open("../src/sprite_rom1.v","w") as fp:
