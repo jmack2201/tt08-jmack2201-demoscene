87187e7c48b602b58fe2a7f3258b76029ae0d606
Jacob Mack <jacobmack2@live.com>
2024-09-06 00:18:13 -0400

    removed two sprite roms

--- src/pixel_color.v
+++ src/pixel_color.v
@@ -10,8 +10,8 @@ module pixel_color (
     reg [5:0] rom1_RGB;
     reg [5:0] rom2_RGB;
     reg [5:0] rom3_RGB;
-    sprite_rom0 rom0 (.clk(clk), .addr(addr), .color_out(rom0_RGB));
-    sprite_rom1 rom1 (.clk(clk), .addr(addr), .color_out(rom1_RGB));
+    // sprite_rom0 rom0 (.clk(clk), .addr(addr), .color_out(rom0_RGB));
+    // sprite_rom1 rom1 (.clk(clk), .addr(addr), .color_out(rom1_RGB));
     sprite_rom2 rom2 (.clk(clk), .addr(addr), .color_out(rom2_RGB));
     sprite_rom3 rom3 (.clk(clk), .addr(addr), .color_out(rom3_RGB));
 
@@ -165,10 +165,10 @@ module pixel_color (
         if (visible) begin
             if (in_sprite) begin
                 case (sprite_sel)
-                    0 : {R,G,B} = rom0_RGB;
-                    1 : {R,G,B} = rom1_RGB;
-                    2 : {R,G,B} = rom2_RGB;
-                    3 : {R,G,B} = rom3_RGB;
+                    // 0 : {R,G,B} = rom0_RGB;
+                    // 1 : {R,G,B} = rom1_RGB;
+                    0 : {R,G,B} = rom2_RGB;
+                    1 : {R,G,B} = rom3_RGB;
                     default: {R,G,B} = rom0_RGB;
                 endcase
             end else begin
